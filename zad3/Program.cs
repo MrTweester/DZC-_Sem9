d8e7a0b0d9c452ab18a60285142127823fd90cc2
Islam Dagaev <spy_002@bk.ru>
2023-06-08 19:38:49 +0500

    дз

---
+++ zad3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+///Метод вычисления функции Аккермана:
+int FunctionAckerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m != 0 && n == 0)
+    {
+        return FunctionAckerman(m - 1, 1);
+    }
+    if (m > 0 && n > 0)
+    {
+        return FunctionAckerman(m - 1, FunctionAckerman(m, n - 1));
+    }
+    return FunctionAckerman(m, n);
+}
+
+Console.Write("Введите начальное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите начальное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Функция Аккермана для чисел A({m},{n}) = {FunctionAckerman(m, n)}");
---
+++ zas2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+void RoadToN(int m, int n, int sum)
+{
+    if (n < m)
+    {
+        System.Console.Write($"Сумма элементов: {sum}");
+        return;
+    }
+
+    sum = sum + (m++);
+    RoadToN(m, n, sum);
+}
+
+System.Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+RoadToN(m, n, 0);
